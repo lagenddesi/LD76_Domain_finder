846d0082d8b58396702dfa03e8430ac830b8514d
lagenddesi <lagenddesi76@gmail.com>
2026-09-06 15:10:53 +0500

    Add connection arguments for SQLite support

--- backend/app/database.py
+++ backend/app/database.py
@@ -10,10 +10,20 @@ class Base(DeclarativeBase):
     pass
 
 
+connect_args = {}
+
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {
+        "check_same_thread": False,
+    }
+
+
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
+    connect_args=connect_args,
 )
+
 
 SessionLocal = sessionmaker(
     bind=engine,
